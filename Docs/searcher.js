5d0ddf46dd8231406715b5e3ed5fad9ac60fad61
OFFSAYING <120654757+Offsaying@users.noreply.github.com>
2023-05-25 08:47:51 +0800

    Add files via upload

---
+++ Docs/searcher.js
@@ -0,0 +1,123 @@
+let SearchResult = '[{"title": "uni", "path": "uni.html", "text": "HelloHelloHelloHello"}, {"title": "test", "path": "test.html", "text": "World"}, {"title": "index", "path": "index.html", "text": ""}]';
+obj = JSON.parse(SearchResult);
+
+function check() {
+    return false;
+}
+
+function maxfor(arr) {
+    var len = arr.length;
+    var max = -Infinity;
+    while (len--) {
+        if (arr[len] > max) {
+            max = arr[len];
+        }
+    }
+    return max;
+}
+
+let input = document.getElementById('input')
+let result = document.getElementById('resultlist')
+let button = document.getElementById('searchbutton')
+
+input.addEventListener("focus", e => {
+    result.id = 'active'
+    result.style.overflow = 'hidden'
+    result.style.padding = '0px'
+    result.style.color="#66cccc"
+    result.style.fontFamily="Verdana"
+    result.style.fontWeight="900"
+    result.style.fontSize="100"
+})
+input.addEventListener("blur", e => {
+    result.id = 'resultlist'
+    result.style.overflow = 'hidden'
+    result.style.padding = '0'
+})
+
+function searchtext() {
+    result.innerHTML = input.value;
+
+
+    // 标题搜索
+    resultcount = 0;
+    resultstr = '';
+    var resulttitlecache = new Array()
+    for (i = 0; i < obj.length; i++) {
+        if (obj[i]['title'].includes(input.value) == true) {
+            resulttitlecache.unshift(obj[i]['title'])
+            resultcount++;
+        }
+    }
+
+    // 标题搜索结果展示
+    if (resultcount !== 0 && resultcount !== obj.length) {
+        for (i = 0; i < resulttitlecache.length; i++) {
+            for (j = 0; j < obj.length; j++) {
+                if (obj[j]['title'] == resulttitlecache[i]) {
+                    titlesearchresult = '<a href="https://offsaying.github.io/OS_Jupiter/Docs/' + obj[j]["path"] + '" style="color:#FAFAD2;font-size:18px">' + obj[j]['title'].replace(new RegExp(input.value, 'g'), '<mark style="background-color:transparent;color:#7cfc00">' + input.value + '</mark>')+'...<br>'
+                    resultstr = titlesearchresult + resultstr
+                }
+            }
+            result.innerHTML = resultstr;
+        }
+    }
+
+    // 正文搜索
+    var resulttextcache = new Array()
+    for (i = 0; i < obj.length; i++) {
+        if (obj[i]['text'].includes(input.value) == true) {
+            resulttextcache.unshift(obj[i]['text'])
+            resultcount++;
+        }
+    }
+
+    // 正文搜索结果计数
+    var targetname = new Array()
+    var targetscore = new Array()
+    if (resulttextcache.length !== 0 && input.value !== '') {
+        for (i = 0; i < resulttextcache.length; i++) {
+            for (j = 0; j < obj.length; j++) {
+                if (obj[j]['text'] == resulttextcache[i]) {
+                    targetname.unshift(obj[j]['title'])
+                    targetscore.unshift(obj[j]['text'].match(RegExp(input.value, 'gim')).length)
+                }
+            }
+        }
+    }
+
+    //排序相关选项
+    var targetscorecache = targetscore.concat([]);
+    var resultfortext = '';
+    var textsearchresult = ''
+    targetscorecache.sort(function (a, b) {
+        return b - a
+    })
+    for (i = 0; i < targetscorecache.length; i++) {
+        for (j = 0; j < targetscore.length; j++) {
+            if (targetscorecache[i] == targetscore[j]) {
+                console.log('文章排序:' + targetname[j])
+                for (k = 0; k < obj.length; k++) {
+                    if (obj[k]['title'] == targetname[j]) {
+                        // 确认选区
+                        textorder = obj[k]['text'].indexOf(input.value) - 15;
+                        while (textorder < 0) {
+                            textorder++
+                        }
+
+                        resultfortext = '<a href="https://offsaying.github.io/OS_Jupiter/Docs/' + obj[k]["path"] + '" style="color:#FAFAD2;font-size:18px">' + obj[k]['title'] + '</a>&nbsp;-&nbsp;' + targetscorecache[i] + '&nbsp;Matched&nbsp;-&nbsp;...' + obj[k]['text'].substring(textorder, textorder + 100).replace(new RegExp(input.value, 'g'), '<mark style="background-color:transparent;color:#7cfc00">' + input.value + '</mark>')+'...<br>'
+                        textsearchresult = textsearchresult + resultfortext;
+                    }
+                }
+            }
+        }
+    }
+
+    if (resultcount !== obj.length) {
+        if (resultcount == 0) {
+            result.innerHTML ='<p>Sorry, we did not find any documents..</p>'
+        }
+    }
+
+    result.innerHTML = result.innerHTML.substring(0, result.innerHTML.length - 4) + textsearchresult.substring(0, textsearchresult.length - 4) 
+}
